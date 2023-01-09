1c28a170e97b22a7579e5b9085fba33a220378e5
s.omelianchuk <s.omelianchuk@viseven.com>
2023-01-09 16:40:59 +0200

    upd(*):

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,16 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    redirect: { name: 'Home' }
+  },
+  {
+    path: '/app-weather/',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/favorites',
+    path: '/app-weather/favorites',
     name: 'favorites',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/FavoritesView.vue')
   }
 ]
